916458d6c380ccdefec490c103c5dc2d45b17545
Sumit Upadhyay <sumit.upadhyay@successive.com>
2022-08-26 19:23:37 +0530

    Removed unwanted code

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
-
 //SELECT ELEMENTS
 //querySelector(): - The querySelector() method returns the first element that matches a CSS selector
 
